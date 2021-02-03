41bba0693bd7c780208b4179d079dbe346bdcecb
Abitharani Jeyachandran <abitharani009@gmail.com>
2021-02-03 14:52:11 +0530

    Remove social links

--- pages/2-1-footer-bar.php
+++ pages/2-1-footer-bar.php
@@ -2,7 +2,7 @@
 <footer id="footer">
   <div class="container">
     <div class="copyright">
-      &copy; <?php echo date('Y'); ?> Copyright <strong><span>s<a target="_blank" href="https://shanjeevan.oceanbio.me/pages/1-0-index.php">hanjeevan.oceanbio.me</a></span></strong>. All Rights Reserved.
+      &copy; <?php echo date('Y'); ?> Copyright <strong><a target="_blank" href="https://shanjeevan.oceanbio.me/pages/1-0-index.php"><span>shanjeevan.oceanbio.me</span></a></strong>. All Rights Reserved.
     </div>
     <div class="credits">
       Co-created by <a target="_blank" href="https://www.linkedin.com/in/abitharani-jeyachandran-8372b6199/">Abitharani.J</a> & <a target="_blank" href="https://www.linkedin.com/in/shanjeevan-amalanathan">Shanjeevan.A</a></br>
